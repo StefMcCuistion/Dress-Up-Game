24bc37c6a8e79f484b4cd1bc8dc9d777b284b649
Stef McCuistion <stef.mccuistion@gmail.com>
2025-01-15 21:38:16 -0600

    The escape key now closes the game. exit() from sys is now used after pg.quit()

--- src/main.py
+++ src/main.py
@@ -15,6 +15,7 @@ def main():
             720
     )       # Window width and height
     pg.init
+    ctypes.windll.user32.SetProcessDPIAware() # keeps windows GUI scale settings from messing with resolution
     display = pg.display.set_mode((W, H))
     pg.display.set_caption('Dress Up Game')
     clock = pg.time.Clock()
@@ -25,7 +26,11 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 running = False
+            if event.type == pg.KEYDOWN:
+                if event.key == pg.K_ESCAPE:
+                    running = False
     pg.quit()
+    exit()
 
 
 

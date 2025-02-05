06e4fca2dcb6526bba446c1395e46fd7f70f3b63
StefMcCuistion <stef.mccuistion@gmail.com>
2025-02-05 16:50:12 -0600

    Reorganized image folder and moved imports to settings module

--- src/main.py
+++ src/main.py
@@ -1,13 +1,3 @@
-import pygame as pg
-from pygame import mixer
-from os.path import join
-import os
-from sys import exit
-import tkinter
-import ctypes
-import pywinauto
-import pyautogui
-from settings import *
 
 class Game():
 
--- src/settings.py
+++ src/settings.py
@@ -1,3 +1,14 @@
+import pygame as pg
+from pygame import mixer
+from os.path import join
+import os
+from sys import exit
+import tkinter
+import ctypes
+import pywinauto
+import pyautogui
+from settings import *
+
 W, H = (
         1280,
         720
